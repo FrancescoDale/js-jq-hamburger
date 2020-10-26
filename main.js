c47d6f5d032cfcf91a86a054a1761ab2271ca6c7
FrancescoDale <fd060862@gmail.com>
2020-10-26 14:54:05 +0100

    corpo della prima funzione

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // funzione che al click sull'icona dell'hamburger menu mostra il menu mobile//
 
-$(".fa-bars").click(function()) {
-    
+$(".fa-bars").click(function() {
+    // al click sull'elemento, mostra il menu
+    $(".hamburger-menu").show();
 }
+) // chiusa la parentesi click
