53a7c73322f1a4066459fe89742bedbc3538beab
FrancescoDale <fd060862@gmail.com>
2020-10-26 15:04:57 +0100

    fix commento

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ $(".fa-bars").click(function() {
 // funzione che quando viene cliccata la x che appare dopo il click sull'icona dell'hamburger menu, chiude tutto e fa tornare le cose come prima del click iniziale
 
 $(".fa-times").click(function() {
+    // al click sulla x, chiude il menu
     $(".hamburger-menu").hide();
 }
 ) // parentesi del click
